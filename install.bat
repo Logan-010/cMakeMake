5cb58b5e5b723792b72993ea6e8d5fac5ec5cc92
Logan <68621340+Logan-010@users.noreply.github.com>
2023-08-08 11:47:12 -0500

    Update install.bat

--- install.bat
+++ install.bat
@@ -6,8 +6,7 @@ set "destination_folder=C:\cmakemake"
 rem Copy the folder
 xcopy "%source_folder%" "%destination_folder%" /E /I
 
-rem Add the destination folder to the PATH
-powershell -Command "$existingPath = [Environment]::GetEnvironmentVariable('PATH', [EnvironmentVariableTarget]::Machine); $newPath = $existingPath + ';C:\cmakemake'; [Environment]::SetEnvironmentVariable('PATH', $newPath, [EnvironmentVariableTarget]::Machine);"
-
-echo "cmakemake installed and added to PATH."
+echo "cmakemake installed, read WARNING.txt to add to path"
 echo "use via cmakemake"
+
+pause
